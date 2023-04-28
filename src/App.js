e6d7a201fabc1600a79b108974e8bad0b92e8b01
llinjae <dldlswognqh@naver.com>
2023-04-28 09:43:48 +0900

    Diary UI, 추가, 삭제

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState, useRef } from 'react';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import './App.css';
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: '이인재',
+//     content: '하이 1',
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: '이인재',
+//     content: '하이 2',
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: '이인재',
+//     content: '하이 3',
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  return (
+    <div className='App'>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onRemove={onRemove} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/DiaryList.js
@@ -0,0 +1,22 @@
+import DiaryItem from './DiaryItem';
+
+const DiaryList = ({ diaryList, onRemove }) => {
+  return (
+    <div className='DiaryList'>
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} onRemove={onRemove} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// undefined로 전달될 것 같은 props가 기본값을 설정할 수 있도록 함.
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
